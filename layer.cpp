81835ae56cddc2b3ca23088732491f617d66dd8c
SantosLeandro <santos.leandrom@outlook.com>
2025-05-04 19:56:18 -0300

    fix when removing gameobjects

--- layer.cpp
+++ layer.cpp
@@ -2,11 +2,12 @@
 #include "global.h"
 #include "level.h"
 
-GameObject::GameObject(){
-    
+GameObject::GameObject()
+{
 }
 
-GameObject::GameObject(std::string name) {
+GameObject::GameObject(std::string name)
+{
     this->name = name;
 }
 
@@ -19,12 +20,10 @@ GameObject::GameObject(std::string name, int x, int y)
 
 Layer::Layer()
 {
-
 }
 
 Layer::~Layer()
 {
-    
 }
 
 int Layer::Width()
@@ -34,13 +33,14 @@ int Layer::Width()
 
 int Layer::Height()
 {
-     return data.size();
+    return data.size();
 }
 
 void Layer::insertTile(int x, int y, int id)
 {
     rollback.push_back(data);
-    if(rollback.size()>100){
+    if (rollback.size() > 100)
+    {
         rollback.pop_front();
     }
     data[y][x] = id;
@@ -49,9 +49,11 @@ void Layer::insertTile(int x, int y, int id)
 std::string Layer::getDataStr()
 {
     std::string tmp = "";
-    for(int i = 0; i < data.size(); i++){
-        for(int j = 0; j < data[0].size(); j++){
-            tmp += std::to_string(data[i][j])+",";
+    for (int i = 0; i < data.size(); i++)
+    {
+        for (int j = 0; j < data[0].size(); j++)
+        {
+            tmp += std::to_string(data[i][j]) + ",";
         }
     }
     tmp.pop_back();
@@ -60,16 +62,18 @@ std::string Layer::getDataStr()
 
 void Layer::undo()
 {
-    if(rollback.size() > 0 ) {
+    if (rollback.size() > 0)
+    {
         data = rollback.back();
         rollback.pop_back();
     }
 }
 
-void Layer::update(int w, int h, const char* name, const char* texture)
+void Layer::update(int w, int h, const char *name, const char *texture)
 {
     data.resize(h);
-    for(int i = 0; i < h; i++){
+    for (int i = 0; i < h; i++)
+    {
         data[i].resize(w);
     }
     this->name = name;
@@ -78,22 +82,24 @@ void Layer::update(int w, int h, const char* name, const char* texture)
 
 void Layer::removeGameObject(int x, int y)
 {
-        int w = 0; 
-        int h = 0;
-        for (int i = 0; i <= gameObjects.size(); ++i) {
-            w = TileSelector::sprites[gameObjects[i].name].w;
-            h = TileSelector::sprites[gameObjects[i].name].h;
-            if (
-                (x >= gameObjects[i].x && x <= (gameObjects[i].x + w)) &&
-                (y >= gameObjects[i].y && y <= (gameObjects[i].y + h))) {
-                
-                // Move the last object to the current position
-                gameObjects[i] = std::move(gameObjects[gameObjects.size() - 1]);
-                // Remove the last object
-                gameObjects.pop_back();
-            }
+    for (size_t i = 0; i < gameObjects.size();)
+    {
+        int w = TileSelector::sprites[gameObjects[i].name].w;
+        int h = TileSelector::sprites[gameObjects[i].name].h;
+        if (
+            (x >= gameObjects[i].x && x <= (gameObjects[i].x + w)) &&
+            (y >= gameObjects[i].y && y <= (gameObjects[i].y + h)))
+        {
+
+            gameObjects[i] = std::move(gameObjects.back());
+            gameObjects.pop_back();
+        }
+        else
+        {
+            ++i;
         }
     }
+}
 
 void Layer::copyTiles(int x1, int y1, int x2, int y2, const Level *level)
 {
@@ -109,7 +115,8 @@ void Layer::copyTiles(int x1, int y1, int x2, int y2, const Level *level)
         int xv = 0;
         for (int j = startW; j <= endW; j++)
         {
-            if(i >= level->layer[TileSelector::layerId].data.size() || j >= level->layer[TileSelector::layerId].data[0].size()){
+            if (i >= level->layer[TileSelector::layerId].data.size() || j >= level->layer[TileSelector::layerId].data[0].size())
+            {
                 continue;
             }
             copyData.push_back(TileBlock(xv * level->tileSize, yv * level->tileSize, level->layer[TileSelector::layerId].data[i][j]));
